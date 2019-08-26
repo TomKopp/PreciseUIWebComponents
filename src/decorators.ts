7b048385fbbcabcbedf2f6578a21c161dd8a24d6
Kopp <tom.kopp@saxsys.de>
2019-08-26 14:52:06 +0200

    update class decorator

--- src/decorators.ts
+++ src/decorators.ts
@@ -1,4 +1,30 @@
+// type Constructor<T> = { new(...args: any[]): {} };
+
+// // From the TC39 Decorators proposal
+// interface ClassElement {
+//   kind: 'field'|'method';
+//   key: PropertyKey;
+//   placement: 'static'|'prototype'|'own';
+//   initializer?: Function;
+//   extras?: ClassElement[];
+//   finisher?: <T extends { new(...args: any[]): {} }>(classConstructor: T) => any;
+//   descriptor?: PropertyDescriptor;
+// }
+
+// // From the TC39 Decorators proposal
+// interface ClassDescriptor {
+//   kind: 'class';
+//   elements: ClassElement[];
+//   finisher?: <T extends { new(...args: any[]): {} }>(classConstructor: T) => any;
+// }
+
 export const defineElement = (name: string, options?: Object) =>
+  (classDescriptor: any) => {
+    classDescriptor.finisher = <T extends { new(...args: any[]): {} }>(classConstructor: T) => customElements.define(name, classConstructor, options);
+    return classDescriptor;
+  }
+
+export const defineElementLegacy = (name: string, options?: Object) =>
   <T extends { new(...args: any[]): {} }>(classConstructor: T) => {
     customElements.define(name, classConstructor, options);
     return classConstructor;
