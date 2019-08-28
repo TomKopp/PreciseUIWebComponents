df813780362964109797345af848f60bf3b0d249
Kopp <tom.kopp@saxsys.de>
2019-08-28 18:09:54 +0200

    remove litElement from test

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
-import { Link } from "./components/Link/link";
 import { TextField } from "./components/TextField/text-field";
 import { Test } from "./components/Base/test";
 import { Card } from "./components/Card/card";
 
 export {
   Test
-  , Link
   , TextField
   , Card
 };
