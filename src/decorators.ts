e2f35774ef2b5cf3753ef9fd1728e60a1f6b6e5f
Kopp <tom.kopp@saxsys.de>
2019-08-30 16:07:40 +0200

    clean up property deorator

--- src/decorators.ts
+++ src/decorators.ts
@@ -10,14 +10,12 @@ export const defineElement = (name: string, options?: Object) =>
 export function property(propertyDeclaration?: PropertyDeclaration) {
   // Second parameter (name) just to please TypeScript...
   return function(propertyDescriptor: any, name?: PropertyKey): any {
-    const value = propertyDescriptor.initializer();
     // propertyDescriptor.initializer = function initializer() {
     //   console.log('initializer: ', this);
     //   return value;
     // }
     propertyDescriptor.finisher = function finisher(classConstructor: typeof BaseElement) {
       classConstructor.addClassProperty(propertyDescriptor.key, propertyDeclaration);
-      // console.log('classConstructor: ', classConstructor, ' value: ', value);
     }
     return propertyDescriptor;
   }
