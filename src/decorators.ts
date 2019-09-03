59513efee9bcc5069505210266e9428d7f8aee34
Kopp <tom.kopp@saxsys.de>
2019-09-03 15:33:27 +0200

    update decorators

--- src/decorators.ts
+++ src/decorators.ts
@@ -3,17 +3,47 @@ import { BaseElement } from './components/Base/base-element';
 
 export const defineElement = (name: string, options?: Object) =>
   function (classDescriptor: any) {
-    classDescriptor.finisher = <T extends { new(...args: any[]): {} }>(classConstructor: T) => customElements.define(name, classConstructor, options);
+    classDescriptor.finisher = function finisher(classConstructor: typeof BaseElement) {
+      customElements.define(name, classConstructor, options);
+    }
     return classDescriptor;
   }
 
 export function property(propertyDeclaration?: PropertyDeclaration) {
   // Second parameter (name) just to please TypeScript...
-  return function(propertyDescriptor: any, name?: PropertyKey): any {
-    // propertyDescriptor.initializer = function initializer() {
-    //   console.log('initializer: ', this);
-    //   return value;
-    // }
+  return function (propertyDescriptor: any, name?: PropertyKey): any {
+    /*
+    If the decorator is invoked on an field property it generates accessors with that name
+    and a somewhat hidden storage property.
+    The accessors are necessary to react on property changes with an render update.
+    */
+    if (propertyDescriptor.kind === 'field') {
+      const propertyKey = `__${propertyDescriptor.key}`;
+      propertyDescriptor.extras = [{
+        key: propertyKey,
+        kind: propertyDescriptor.kind,
+        placement: propertyDescriptor.placement,
+        initializer: propertyDescriptor.initializer,
+        descriptor: {
+          configurable: true,
+          enumerable: true,
+          writable: true
+        }
+      }];
+
+      propertyDescriptor.kind = 'method';
+      propertyDescriptor.placement = 'prototype';
+      delete propertyDescriptor.initializer;
+      propertyDescriptor.descriptor = {
+        get() { return this[propertyKey]; },
+        set(val: any) {
+          this[propertyKey] = val;
+          this.requestUpdate();
+        },
+        configurable: true,
+        enumerable: true
+      };
+    }
     propertyDescriptor.finisher = function finisher(classConstructor: typeof BaseElement) {
       classConstructor.addClassProperty(propertyDescriptor.key, propertyDeclaration);
     }
